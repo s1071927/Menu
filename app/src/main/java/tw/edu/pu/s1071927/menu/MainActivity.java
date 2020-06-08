15645744417c2999118b55f40a21a668a7f5e4ff
s1071927 <s1071927@gm.pu.edu.tw>
2020-06-08 21:21:25 +0800

    menu1

---
+++ app/src/main/java/tw/edu/pu/s1071927/menu/MainActivity.java
@@ -0,0 +1,28 @@
+package tw.edu.pu.s1071927.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //設定全螢幕顯示
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //設定螢幕為橫式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+
+        setContentView(R.layout.activity_main);
+    }
+}
